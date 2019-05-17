e466a765078426a7aa5e5de7d6537b8cbeebe04d
DarrenMun <46787511+DarrenMun@users.noreply.github.com>
2019-05-17 15:58:15 +0200

    Update entry_point.sh

--- Docker_emulator/entry_point.sh
+++ Docker_emulator/entry_point.sh
@@ -5,7 +5,7 @@
 #============================================
 ${ANDROID_HOME}/platform-tools/adb start-server
 ${ANDROID_HOME}/emulator/emulator -avd darren -no-audio -no-boot-anim -no-window -no-accel -gpu off -timezone Africa/Johannesburg &
-${ANDROID_HOME}/platform-tools/adb adb wait-for-device devices
+${ANDROID_HOME}/platform-tools/adb wait-for-device devices
 
 #============================================
 #Starts Appium
