41e2566a367305f8149103e9b313f95e1fa5b89e
DarrenMun <46787511+DarrenMun@users.noreply.github.com>
2019-05-17 15:53:25 +0200

    Update entry_point.sh

--- Docker_emulator/entry_point.sh
+++ Docker_emulator/entry_point.sh
@@ -4,8 +4,8 @@
 #Starts Android emulator in headless mode
 #============================================
 ${ANDROID_HOME}/platform-tools/adb start-server
-${ANDROID_HOME}/emulator/emulator -avd darren -no-audio -no-boot-anim -no-window -verbose -no-accel -gpu off -timezone Africa/Johannesburg &
-${ANDROID_HOME}/platform-tools/adb devices
+${ANDROID_HOME}/emulator/emulator -avd darren -no-audio -no-boot-anim -no-window -no-accel -gpu off -timezone Africa/Johannesburg &
+${ANDROID_HOME}/platform-tools/adb adb wait-for-device devices
 
 #============================================
 #Starts Appium
