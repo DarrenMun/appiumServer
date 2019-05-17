eea19767472f3b45875c54a746410f1c6351023d
DarrenMun <46787511+DarrenMun@users.noreply.github.com>
2019-05-17 12:17:14 +0200

    Update entry_point.sh

--- Docker_emulator/entry_point.sh
+++ Docker_emulator/entry_point.sh
@@ -3,8 +3,8 @@
 #============================================
 #Starts Android emulator in headless mode
 #============================================
-
-${ANDROID_HOME}/emulator/emulator -avd darren -no-audio -no-boot-anim -no-window -verbose -no-accel -gpu off -timezone Africa/Johannesburg -qemu -vnc :2
+${ANDROID_HOME}/platform-tools/adb start-server
+${ANDROID_HOME}/emulator/emulator -avd darren -no-audio -no-boot-anim -no-window -verbose -no-accel -gpu off -timezone Africa/Johannesburg -qemu -vnc :2 &
 ${ANDROID_HOME}/platform-tools/adb wait-for-device
 
 #============================================
