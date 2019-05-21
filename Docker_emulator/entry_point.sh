ce0aaf5dbf55f241637a3d9dce7dd9867d81d864
DarrenMun <46787511+DarrenMun@users.noreply.github.com>
2019-05-21 09:58:23 +0200

    Update entry_point.sh

--- Docker_emulator/entry_point.sh
+++ Docker_emulator/entry_point.sh
@@ -6,7 +6,7 @@
 #${ANDROID_HOME}/platform-tools/adb start-server
 ${ANDROID_HOME}/emulator/emulator -avd darren -gpu off -no-accel -no-boot-anim -no-window -no-audio -timezone Africa/Johannesburg &
 #-gpu off -no-audio -no-accel -timezone Africa/Johannesburg
-${ANDROID_HOME}/platform-tools/adb wait-for-device
+${ANDROID_HOME}/platform-tools/adb wait-for-device start-server
 ${ANDROID_HOME}/platform-tools/adb devices
 
 #============================================
